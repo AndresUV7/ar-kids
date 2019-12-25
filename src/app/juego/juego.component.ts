6087e260880e975ee18b428bcbfc0ca605d85fcc
AndresUV7 <andres.ullauri18@gmail.com>
2019-12-25 16:08:03 -0500

    probando touch

---
+++ src/app/juego/juego.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import Phaser from 'phaser';
+
+class NewScene extends Phaser.Scene {
+
+  
+  constructor() {
+    super({ key: 'new' });
+  }
+
+  preload() {
+    // console.log('enter preload');
+    this.load.image("pajaro","/assets/img/bird.png");
+  }
+
+  create() {
+    // console.log('enter create');
+    let pajaro=this.add.image(50,100,"pajaro").setInteractive();
+
+    this.input.setDraggable(pajaro);
+
+    this.input.on('dragstart', function (pointer, gameObject) {
+
+      this.children.bringToTop(gameObject);
+
+    }, this);
+
+    this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+  
+        gameObject.x = dragX;
+        gameObject.y = dragY;
+  
+    });
+    }
+
+  update(time, delta){
+    console.log(delta);
+  }
+}
+
+@Component({
+  selector: 'app-juego',
+  templateUrl: './juego.component.html',
+  styleUrls: ['./juego.component.css']
+})
+export class JuegoComponent implements OnInit {
+
+  phaserGame: Phaser.Game;
+  config: Phaser.Types.Core.GameConfig;
+
+  constructor() {
+    this.config = {
+      type: Phaser.AUTO,
+      scene: [ NewScene ],
+      physics: {
+        default: 'arcade',
+      },
+      scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        parent: 'gameContainer',
+        width: 1024,
+        height: 768,
+      }
+    };
+  }
+
+  ngOnInit() {
+    this.phaserGame = new Phaser.Game(this.config);
+  }
+
+}
