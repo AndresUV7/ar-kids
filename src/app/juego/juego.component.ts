80bacd4a0ba79475329491e14d5d727dfc376d0d
AndresUV7 <andres.ullauri18@gmail.com>
2020-01-11 15:28:24 -0500

    cambiando a phaser

--- src/app/juego/juego.component.ts
+++ src/app/juego/juego.component.ts
@@ -16,7 +16,7 @@ class NewScene extends Phaser.Scene {
 
   preload() {
     // console.log('enter preload');
-    this.load.path = '/assets/img/'
+    this.load.path = '/ar-kids/assets/img/'
     this.load.image("pajaro1","bird.png");
     this.load.image("pajaro2","bird_dos.png");
     
